a99dbd354c42ecb0e76a1d795bf04bb167058141
Remy Ripple <cripple@Remys-computer.local>
2016-09-25 12:59:13 -0500

    shakeyshake

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 $(function(){
 
+//making everthing bounce like there could be monsters fighting in the bg
+//if you wanna turn this off so you can work just comment it out. all this is doing is using the animate.css thing that i put in the head on the index. if you wanna look more into it, they have a git page for documentation: https://github.com/daneden/animate.css
+  $('.castleMain').addClass('animated bounce infinite');
+  $('.castleleftleft').addClass('animated bounce infinite');
+  $('.castleLeft').addClass('animated bounce infinite');
+  $('.castleRight').addClass('animated bounce infinite');
+  $('.castlerightright').addClass('animated bounce infinite');
+  $('.leftbehind').addClass('animated bounce infinite');
+  $('.rightbehind').addClass('animated bounce infinite');
+  $('.behindbehind').addClass('animated bounce infinite');
+
+
 
 
 
